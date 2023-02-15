bbfe6b002d4a235f79a618aa7ade2b5e4d4b6314
BegeBordeaux <104512853+BegeBordeaux@users.noreply.github.com>
2023-02-15 18:24:01 -0500

    Find merkle root for niceList

--- client/index.js
+++ client/index.js
@@ -5,13 +5,17 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
+  let tree = new MerkleTree(niceList)
+  console.log(tree.getRoot())
+  
   // TODO: how do we prove to the server we're on the nice list? 
-
+  
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
   });
-
+  
   console.log({ gift });
 }
+
 
 main();
