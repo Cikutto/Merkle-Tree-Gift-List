058c4361522aad7d70336407cab6f5d1386bb04e
BegeBordeaux <104512853+BegeBordeaux@users.noreply.github.com>
2023-02-15 19:34:14 -0500

    prove niceList names to server + add body parameters

--- client/index.js
+++ client/index.js
@@ -5,13 +5,22 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  let tree = new MerkleTree(niceList)
-  console.log(tree.getRoot())
   
+  const tree = new MerkleTree(niceList)
+  console.log(tree.getRoot())
+
   // TODO: how do we prove to the server we're on the nice list? 
+  const root = tree.getRoot();
+  const niceListNames = process.argv.slice(2);
+  const name = niceListNames.join(" ");
+  const index = niceList.findIndex(n => n === name);
+  const proof = tree.getProof(index);
   
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name,
+    proof,
+    root
   });
   
   console.log({ gift });
